f97b14aaeec762257c3c3542f39e23f773d1c8d4
Krystian Prus <lamerhiob@gmail.com>
2022-05-11 17:38:43 +0200

    ready

--- src/rotatex.ts
+++ src/rotatex.ts
@@ -1,5 +1,4 @@
 import './rotatex.scss';
-import { NodeMap, DemoMap } from './nodemap';
 
 interface RotatexOptions {
     name: string
@@ -8,35 +7,23 @@ interface RotatexOptions {
 export class Rotatex {
     private static readonly ObserverOptions: MutationObserverInit = { attributes: false, childList: true, subtree: false };
     
-    private observer: MutationObserver = new MutationObserver((mutations: MutationRecord[], observer: MutationObserver) => {
-        for (let mutation of mutations) {
-            let node: Node = mutation.target;
-            let options: RotatexOptions = this.elements.get(node);
-            this.build(node, options);
-        }
+    private observer: MutationObserver = new MutationObserver(() => {
+        this.build();
     });
 
-    // private elements: NodeMap<RotatexOptions> = new NodeMap<RotatexOptions>();
-    private elements: DemoMap = new DemoMap();
+    private build() {
+        this.element.classList.add('rotatex');
+        let agle: number = 360 / this.element.children.length;
 
-    build(node: Node, options: RotatexOptions) {
-        console.log(node, options);
-    }
-
-    on(element: HTMLElement, options: RotatexOptions) {
-        this.observer.observe(element, Rotatex.ObserverOptions);
-        this.elements.set(element, options);
-        this.build(element, options);
-    }
-
-    off(element: HTMLElement) {
-        if (this.elements.has(element)) {
-            this.elements.delete(element);
-            this.observer.disconnect();
-
-            // for (let item of this.elements.nodes) {
-            //     this.observer.observe(item, Rotatex.ObserverOptions);
-            // }
+        for (let idx: number = 0; idx < this.element.children.length; idx++) {
+            let deg = Math.round(agle * idx);
+            let child: HTMLElement = this.element.children[idx] as HTMLElement;
+            child.style.setProperty('--start-angle', deg + "deg");
         }
+    }
+
+    constructor(private element: HTMLElement, private options?: RotatexOptions) {
+        this.observer.observe(element, Rotatex.ObserverOptions);
+        this.build();
     }
 }
