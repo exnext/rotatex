b0c54b9a9dce2841c9779408a4c8b106cf18fd64
Krystian Prus <lamerhiob@gmail.com>
2022-05-12 11:34:03 +0200

    refactoring and cleaning

--- src/rotatex.ts
+++ src/rotatex.ts
@@ -14,10 +14,10 @@ export class Rotatex {
     private build() {
         this.element.classList.add('rotatex');
         let children: NodeListOf<HTMLElement> = this.element.querySelectorAll(':scope > *:not(.r-off)');
-        let agle: number = 360 / children.length;
+        let angle: number = 360 / children.length;
 
         children.forEach((child: HTMLElement, idx: number) => {
-            let deg = Math.round(agle * idx);
+            let deg = Math.round(angle * idx);
             child.style.setProperty('--start-angle', deg + 'deg');
         });
     }
