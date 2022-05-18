b00d9c7bea50c7268ebf0af89f11f62c7a38efe7
Krystian Prus <lamerhiob@gmail.com>
2022-05-18 15:59:38 +0200

    fix path

--- webpack.config.js
+++ webpack.config.js
@@ -95,7 +95,6 @@ module.exports = {
             template: "./src/index.ejs",
             scriptLoading: "blocking",
             inject: "head",
-            publicPath: "./",
             templateParameters: {
                 name: package.name,
                 version: package.version
